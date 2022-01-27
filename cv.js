d02d1456b8aabde8afa451d0fe53897fe4232cfa
Maylis Gaillard <maylisgaillard@hotmail.fr>
2022-01-27 16:21:56 +0100

    création fenetre modale

--- cv.js
+++ cv.js
@@ -1,8 +1,22 @@
-let cv = document.getElementById("telechargement");
+let cv = document.getElementById("cv");
+
+let div = document.getElementById("modale");
+
+let btnFermeture = document.getElementById("fermerCv");
 
 cv.addEventListener("click", fenetreCV);
 
 
 function fenetreCV() {
-    window.open('img/Cv_Maylis_Gaillard.png');
+    // window.open('img/Cv_Maylis_Gaillard.png');
+    div.classList.remove("cacher");
+    div.classList.add("affiche");
+}
+
+btnFermeture.addEventListener("click", fermerCV);
+
+function fermerCV() {
+    // window.open('img/Cv_Maylis_Gaillard.png');
+    div.classList.remove("affiche");
+    div.classList.add("cacher");
 }
