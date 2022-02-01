3052560ebf620533be91e0a8a25601590f0cd4aa
Maylis Gaillard <maylisgaillard@hotmail.fr>
2022-02-01 15:57:59 +0100

    Début contrôle du formulaire

---
+++ controleFormContact.js
@@ -0,0 +1,8 @@
+let contactForm = document.getElementById("form");
+
+let listeInputs = document.querySelectorAll("input, textarea");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+});
