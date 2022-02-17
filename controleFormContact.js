e97a44d68a1e20921dfaec5c4053f201a47197d9
Maylis Gaillard <maylisgaillard@hotmail.fr>
2022-02-17 19:07:04 +0100

    début contrôle de saisie du formulaire de contact

--- controleFormContact.js
+++ controleFormContact.js
@@ -1,8 +1,44 @@
-// let contactForm = document.getElementById("form");
+let contenuForm = document.getElementsByClassName("champs");
 
-// let listeInputs = document.querySelectorAll("input, textarea");
+let form = document.getElementById("form");
 
-// contactForm.addEventListener("submit", function (event) {
-//     event.preventDefault();
+let listInput = document.querySelectorAll("input, textarea");
 
-// });
+form.addEventListener("submit", valide);
+
+for (elements of contenuForm) {
+    let messErreurs = document.createElement("p");
+    messErreurs.classList.add("nonValide");
+    elements.append(messErreurs);
+}
+
+function valide(event) {
+    event.preventDefault();
+    for (input of listInput) {
+        input.parentNode.lastElementChild.innerText = "";
+        switch (input.type) {
+            case "text":
+                if (input.value == "") {
+                    input.parentNode.lastElementChild.innerText = "Veuillez renseigner votre nom";
+                }
+                break;
+            case "tel":
+                if (input.value == "") {
+                    input.parentNode.lastElementChild.innerText = "Veuillez renseigner votre numéro de téléphone";
+                }
+                break;
+            case "email":
+                if (input.value == "") {
+                    input.parentNode.lastElementChild.innerText = "Veuillez renseigner votre adresse mail";
+                }
+                break;
+            case "textarea":
+                if (input.value == "") {
+                    input.parentNode.lastElementChild.innerText = "Veuillez écrire le motif de votre demande";
+                }
+                break;
+            default: "erreur champs non valides";
+                break;
+        }
+    }
+}
